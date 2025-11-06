834201b4595e9959e0e51bf478f96385d9870ccf
anandarosyid <rosyidananda28@gmail.com>
2025-11-06 20:20:54 +0700

    modifikasi soal 3

--- ArrayRataNilai05.java
+++ ArrayRataNilai05.java
@@ -2,19 +2,31 @@ import java.util.Scanner;
 public class ArrayRataNilai05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukan jumlah mahasiwa : ");
+        int jmlhMhs = sc.nextInt();
+
+        int[] nilaiMhs = new int[jmlhMhs];
+        double totalLulus = 0, totalTdkLulus = 0;
+        double rata2lulus, rata2TdkLulus;
+        int jmlhLulus = 0, jmlhTdkLulus = 0;
 
         for (int i = 0; i <nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
             nilaiMhs[i] = sc.nextInt();
         } for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                jmlhLulus++;
+            } else {
+                totalTdkLulus += nilaiMhs[i];
+                jmlhTdkLulus++;
+            }
         }
-
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rata nilai " + rata2);   
+        rata2lulus = totalLulus / jmlhLulus;
+        rata2TdkLulus = totalTdkLulus / jmlhTdkLulus;
+   
+        System.out.println("Rata-rata nilai lulus = " + rata2lulus); 
+        System.out.println("rata rata nilai tidak lulus = " + rata2TdkLulus);  
 
      
       
